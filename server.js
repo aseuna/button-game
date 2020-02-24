710151d92b42ceaad4a7536d5221d4278d19e4a2
aseuna <arttuseuna@gmail.com>
2020-02-24 21:12:48 +0200

    env and static setup

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const db = require('./lib/db');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+//var distDir = __dirname + "/dist/";
+//app.use(express.static(distDir));
 
 var server = app.listen(process.env.PORT, function() {
     var host = server.address().address;
