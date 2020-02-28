756c817348a315f14558e321eab147b6eac84c2a
aseuna <arttuseuna@gmail.com>
2020-02-28 06:12:31 +0200

    postgres query implementation, frontend button and click decrement

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { Pool } = require('pg');
 
-const db = require('./lib/db');
 require('dotenv').config();
 
 const app = express();
@@ -13,7 +12,41 @@ app.use(bodyParser.json());
 //var distDir = __dirname + "/dist/";
 //app.use(express.static(distDir));
 
-var server = app.listen(process.env.PORT, function() {
+const pool = new Pool({
+    connectionString: process.env.CONNECTIONSTRING
+});
+
+app.post('/api/button_game', async (req, res) => {
+
+    // IMPLEMENT NESTED QUERY
+    const text = 'UPDATE bg_num SET total_number = total_number + 1 WHERE id = 1;';
+    //const text = 'SELECT * FROM bg_num;';
+    try {
+        const result = await pool.query(text)
+
+        console.log(result);
+
+        const j = {
+            incrementation: "SUCCESS"
+        }
+        res.json(j);
+    } catch (err) {
+        console.log(err.stack)
+        const j = {
+            incrementation: "FAILURE"
+        }
+        res.json(j);
+    }
+
+});
+
+/*
+app.get('*', (req,res) =>{
+    console.log(res);
+    res.sendFile(path.join(__dirname+'/../dist/index.html'));
+});*/
+
+var server = app.listen(process.env.SPORT, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="boxContainer" id="mainContainer">
         
-        <b-button 
+        <b-button @click="onSubmit"
         variant="success"
         >Press Here
         </b-button>
 
+        <div id="remainingClicks">Your remaining clicks: {{remainingClicks}}</div>
     </div>
 </template>
 
 <script>
+
+//const Swal = require('sweetalert2');
 
 export default {
     name: 'MainView',
@@ -19,14 +22,49 @@ export default {
     },
     data(){
         return{
-            
+            remainingClicks: 20
         }
     },
     mounted(){
-
+        if(localStorage.remainingClicks) this.remainingClicks = localStorage.remainingClicks;
     },
     methods:{
-      
+        onSubmit: function(){
+
+            // sending http request to backend
+            // at this point any post request t
+            const config = {
+                method: 'POST',
+                headers: {
+                    'content-type':'application/json'
+                }
+            };
+
+            fetch('http://localhost:3000/api/button_game', config)
+            .then(response => response.json())
+            .then(data => {
+
+                // eslint-disable-next-line no-console
+                console.log(data.incrementation);
+            })
+            .catch(function(error){
+                // eslint-disable-next-line no-console
+                console.log(error);
+            });
+
+            if(this.remainingClicks > 0){
+                this.remainingClicks = this.remainingClicks - 1;
+                if(this.remainingClicks === 0){
+                    // implement alert dialog
+                }
+            }
+        }
+    },
+    watch: {
+        remainingClicks: function(clicks) {
+            localStorage.remainingClicks = clicks;
+            console.log("clicks: " + clicks);
+        }
     }
 }
 </script>
